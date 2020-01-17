6216c4f8b0565120e5f894126df16b055a1fe84e
jeor0980 <jesusotovar@hotmail.com>
2020-01-17 14:28:06 -0700

    Basic Auth and Rate Limiting

--- app.py
+++ app.py
@@ -1,17 +1,48 @@
 import re
+from datetime import date
+from redis import Redis
 from eve import Eve
 from eve.io.mongo import Validator
+from eve.auth import BasicAuth
+
+
+
+class PeopleAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        return username == 'admin' and password == 'secret'
 
 
 class MyValidator(Validator):
     def _validate_isodd(self, isodd, field, value):
+        """ Define a brand new 'isodd' rule """
         if isodd and not bool(value & 1):
             self._error(field, 'Value must be an odd number')
 
-    def _validate_type_email(self, field, value):
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
-            self._error(field, 'Value is not a valid email')
+    def _validate_type_email(self, value):
+        """ Extend types by adding a new 'email' type """
+        if re.match(r"[^@]+@[^@]+\.[^@]+", value):
+            return True
 
 
-app = Eve(validator=MyValidator)
- 
+def inject_signature(resource, response):
+    """ This callback is executed every time a document (item)
+    is fetched from the db and it is about to be returned to the client.
+    """
+    response['_signature'] = "Talk Python Training"
+
+
+def add_age(resource, items):
+    """ This callback is invoked every time a POST request hits the service,
+    *before* the documents are sent to the db.
+    """
+    for item in items:
+        if 'born' in item:
+            today = date.today()
+            item['age'] = today.year - item['born'].year
+
+app = Eve(validator=MyValidator, redis=Redis())
+
+# attach a callback function to GET requests.
+app.on_fetched_item += inject_signature
+# attach a callback function to POST requests.
+app.on_insert += add_age
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+from app import PeopleAuth
 # X_DOMAINS = ['example.com','mysite.org','']
 X_DOMAINS = '*'
 
@@ -5,7 +6,37 @@ MONGO_URI = 'mongodb://localhost:27017/eve-course'
 
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+# DATE_FORMAT = '%d $b %Y'
+# QUERY_WHERE = 'find'
+# QUERY_SORT = 'orderby'
+# Disable filters
+# ALLOWED_FILTERS = []
+# SORTING = False
+# MONGO_QUERY_BLACKLIST = []
+# QUERY_PAGE = 'section'
+# MAX_QUERY_RESULT = 'max_results'
+# PAGINATION_DEFAULT = 25
+# PAGINATION_LIMIT = 50
+# OPTIMIZE_PAGINATION_FOR_SPEED = True
+# PROJECTION = False
+# QUERY_PROJECTION = 'fields'
+# IF_MATCH = False
+# ENFORCE_IF_MATCH = False
+# ETAG = 'etag'
+# HATEOAS = True
+# LINKS = 'links'
+
+# num of req, secs
+RATE_LIMIT_GET = (1, 60)
+
+JSON_SORT_KEYS = True
+
+# PAGINATION = False
+
 schema = {
+    # 'allowed_filters': ['lastname'],
+    # 'sorting': False,
     'firstname': {
         'type': 'string',
         'minlength': 1,
@@ -32,19 +63,22 @@ schema = {
     },
     'location': {
         'type': 'dict',
-        'required': True,
+        'required': False,
         'schema': {
             'address': {'type': 'string'},
             'city': {'type': 'string', 'required': True}
         }
     },
-    'email': {
-        'type': 'email',
-    }
+    'email': {'type': 'email'}
 }
 
 DOMAIN = {
     'people': {
+        'authentication': PeopleAuth,
+        'datasource': {'projection': {'lastname': 0}},
+        'projection': True,
+        'pagination': True,
+        # 'hateoas': False,
         'schema': schema
     },
     'works': {
